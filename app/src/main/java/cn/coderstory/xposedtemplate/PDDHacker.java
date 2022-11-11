da5b73a74ec16e76a8807674e4c891f2355e276a
ppfirework <3212329718@qq.com>
2022-11-11 15:27:57 +0800

    upload to github

---
+++ app/src/main/java/cn/coderstory/xposedtemplate/PDDHacker.java
@@ -0,0 +1,47 @@
+package cn.coderstory.xposedtemplate;
+
+import de.robv.android.xposed.*;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class PDDHacker implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam param) throws Throwable {
+        if (!param.packageName.contains("rdbookl")) {
+            return;
+        }
+        ClassLoader classLoader = param.classLoader;
+        XposedHelpers.findAndHookMethod("com.rdbookl.booknie.model.remote.GetVideoModel$DataBean", classLoader, "getGold", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                param.setResult("0");
+                super.beforeHookedMethod(param);
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+            }
+        });
+        XposedHelpers.findAndHookMethod("com.rdbookl.booknie.model.remote.DynamicDetailModel$DataBean", classLoader, "getNeedpay", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                param.setResult(0);
+                super.beforeHookedMethod(param);
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+            }
+        });
+        XposedHelpers.findAndHookMethod("com.rdbookl.booknie.model.remote.GetVideoModel$DataBean", classLoader, "getGold", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                param.setResult(0);
+                super.beforeHookedMethod(param);
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+            }
+        });
+    }
+}
